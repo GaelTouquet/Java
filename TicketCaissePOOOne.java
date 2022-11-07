58c89e18f1c81975fe279f84a708676dd3871831
Gael Touquet <gael.touquet@gmail.com>
2022-11-07 16:38:27 +0100

    little exercise

--- TicketCaissePOOOne.java
+++ TicketCaissePOOOne.java
@@ -1,59 +1,60 @@
 import java.util.*;
 import java.util.ArrayList.*;
 
-
-class Ligne
-{
+class Ligne {
     // variables d'instance'
-    String libel            = "";
-    Double prixUnitaire     = 0.0;
-    Double quantite         = 0.0;
-    Double prixTot          = 0.0;
+    String libel = "";
+    Double prixUnitaire = 0.0;
+    Double quantite = 0.0;
+    Double prixTot = 0.0;
 
     // variable de classe
-    static Integer nbrLigne = 0;  
+    static Integer nbrLigne = 0;
 
-    public Ligne( String nom, Double pu, Double qte   )
-    {
-        this.libel          = nom;        
-        this.prixUnitaire   = pu;        
-        this.quantite       = qte;        
-        this.prixTot  = this.quantite * this.prixUnitaire;
+    public Ligne(String nom, Double pu, Double qte) {
+        this.libel = nom;
+        this.prixUnitaire = pu;
+        this.quantite = qte;
+        this.prixTot = this.quantite * this.prixUnitaire;
         nbrLigne++;
-    } 
+    }
 
-    public void affLigne()
-    {
-        System.out.println(  String.format("%15s %8.2f %8.2f = %8.2f",   this.libel, this.prixUnitaire, this.quantite, this.prixTot     ) );
+    public void affLigne() {
+        System.out.println(
+                String.format("%15s %8.2f %8.2f = %8.2f", this.libel, this.prixUnitaire, this.quantite, this.prixTot));
     }
 }
 
-
-class Ticket
-{
+class Ticket {
     // variables d'instance'
-    String client            = "";
-    Double prixTot           = 0.0;
-    ArrayList<Ligne> lignes  = new ArrayList();
+    String client = "";
+    Double prixTot = 0.0;
+    int nArticle = 0;
+    ArrayList<Ligne> lignes = new ArrayList<Ligne>();
+    int iTicket;
+    static int nTicket = 0;
 
-    public Ticket( )
-    {
-    } 
-
-    public void addAchat(  Ligne l  )
-    {
-        lignes.add( l );
+    public Ticket() {
+        nTicket += 1;
+        this.iTicket = nTicket;
     }
 
-    public void afffiche()
-    {
-        System.out.println(  "============================= " );
-        System.out.println(  String.format("client : %15s",   this.client  ) );
-        for (Ligne l : lignes ) 
-        {
+    public void addAchat(Ligne l) {
+        this.lignes.add(l);
+        this.prixTot += l.prixTot;
+        this.nArticle += l.quantite;
+    }
+
+    public void afffiche() {
+        System.out.println("============================= ");
+        System.out.println(String.format("client : %15s", this.client));
+        for (Ligne l : lignes) {
             l.affLigne();
         }
-        System.out.println(  "============================= " );
+        System.out.println(String.format("nb articles : %2d", this.nArticle));
+        System.out.println(String.format("total : %8.2f", this.prixTot));
+        System.out.println(String.format("ticket %2d / %2d", this.iTicket, nTicket));
+        System.out.println("============================= ");
     }
 }
 
@@ -63,23 +64,20 @@ class TicketCaissePOOOne {
     public static void main(String[] args) {
 
         Ticket t1 = new Ticket();
- 
-        t1.addAchat( new Ligne("romarin", 7.56, 1.0 ) );
-        t1.addAchat( new Ligne("romarin", 7.56, 1.0) );
-        t1.addAchat( new Ligne("rutabaga", 1.5, 5.0));
-        t1.addAchat( new Ligne("choux de Brx", 4.50, 1.0));
-        t1.addAchat( new Ligne("réglisse", 8.75, 0.5));
-        t1.addAchat( new Ligne("bettes", 2.4, 1.0));
+        t1.addAchat(new Ligne("romarin", 7.56, 1.0));
+        t1.addAchat(new Ligne("rutabaga", 1.5, 5.0));
+        t1.addAchat(new Ligne("choux de Brx", 4.50, 1.0));
+        t1.addAchat(new Ligne("réglisse", 8.75, 0.5));
+        t1.addAchat(new Ligne("bettes", 2.4, 1.0));
         t1.afffiche();
 
-
         Ticket t2 = new Ticket();
-        t2.addAchat( new Ligne("livre", 7.56, 1.0 ) );
-        t2.addAchat( new Ligne("essence", 7.56, 1.0) );
-        t2.addAchat( new Ligne("fil à coudre", 1.5, 5.0));
-        t2.addAchat( new Ligne("banane", 4.50, 1.0));
+        t2.addAchat(new Ligne("livre", 7.56, 1.0));
+        t2.addAchat(new Ligne("essence", 7.56, 1.0));
+        t2.addAchat(new Ligne("fil à coudre", 1.5, 5.0));
+        t2.addAchat(new Ligne("banane", 4.50, 1.0));
         t2.afffiche();
 
-        System.out.println(  String.format("nombre de lignes :  %d ", Ligne.nbrLigne  ) );
+        System.out.println(String.format("nombre de lignes :  %d ", Ligne.nbrLigne));
     }
 }
